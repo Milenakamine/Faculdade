04addecde06cd678a6761584ecc4b09a65a99139
Milena Akamine <aakaminemilena@gmail.com>
2022-03-31 02:02:23 -0300

    aulas

---
+++ PYTHON_PROGRAMAÇÃO DE COMPUTADORES - 1S/1.[29.03] - PROGRAMAÇÃO DE COMPUTADORES/atividade.py
@@ -0,0 +1,26 @@
+print ("Primeiro exercicio")
+nome = input ("Digite o seu nome completo:")
+print("Nome: ", nome)
+
+print ("\nSegundo exercicio")
+print("O resultado da conta (10 + 20 x 30) resulta em:", 10+20*30)
+print ("O resultado da conta (4²/30) resulta em:" ,(4**2)/30)
+print ("O resultado da conta((9^4+2)*6-1) resulta em:" ,((9**4)+2)*6-1)
+print ("\nTerceiro exercicio")
+#print ("O resultado da conta (10%3x10²+1-10x4/2) resulta em:", (10/100(3*(10**2)+1 -(10*(4/2)))) )
+
+print ("\n Quarto exercicio")
+a = 3
+b = 5
+print ("O resultado da conta é: ", ((2*a)*(3*b)) )
+
+print ("\nQuinto exercicio")
+a = 10
+b = 15
+c = 20
+print (a+b+c)
+
+print ("\nSexto exercicio")
+salario = 750.00
+aumento = (salario*15)/100
+print (salario+aumento)
