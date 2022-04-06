c5aa892da41cf2d18b5ac9bb08630b5540722a52
Milena Akamine <64390051+Milenakamine@users.noreply.github.com>
2022-04-05 21:49:10 -0300

    Add files via upload

---
+++ PYTHON_PROGRAMAÇÃO DE COMPUTADORES/3.[05.04] E_SE/3.IfAndElse.py
@@ -0,0 +1,9 @@
+a = int(input("Entre com o primeiro valor: "))
+b = int(input("Entre com o segundo valor: "))
+if a>b:
+    print('O primeiro valor é o maior.')
+elif a==b:
+    print('Os números são iguais.')
+    
+else:
+    print('O segundo valor é o maior.')
