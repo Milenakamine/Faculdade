bdb55fac5f05e5504e29d3fefcb1e370bb778908
Milena Akamine <aakaminemilena@gmail.com>
2022-05-07 19:21:30 -0300

    1.1

---
+++ PYTHON_PROGRAMAÇÃO DE COMPUTADORES/1.1/1.6.py
@@ -0,0 +1,4 @@
+#variavel
+salario = 2000
+aumento = 10
+print("O salário atual é de R$:", salario, "\n" "O salário juntamente ao aumento será de R$:", salario+(salario*aumento/100))
---
+++ PYTHON_PROGRAMAÇÃO DE COMPUTADORES/1.1/1.7.py
@@ -0,0 +1,7 @@
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+soma = a+b
+print("A soma dos valores e de:", soma)
+
+#O input recebe as informações como string, então é necessário
+#avisar ao programa que o dado que será recebido é um int ou float 
